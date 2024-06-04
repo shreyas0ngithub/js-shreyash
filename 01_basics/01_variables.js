e1e2aa6fd58237ea70a3fb00cb6c4de2110a1728
shreyas0ngithub <shreyas.shastrakar07@gmail.com>
2024-06-04 09:21:57 +0000

    day_02

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 44854
+let accountEmail = "abc@hemail.com"
+var accounttype = "Savings"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accounttype])
+
+//accountId = 123456
+
+console.log(accountId);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+ 
+ //Data types in js
+ //numberbigInt
+//string
+//boolean
+//null -> standalone value
+//undefined
+//symbol -> unique
+
+
+//object
+
+let naame="shreyas"
+let age=20
+let alive = true
+let partner = null
+let income = undefined
+
+console.table([naame,age,alive,partner,income])
+
+console.log(typeof age); 
+console.log(typeof income)
+console.log(typeof partner)
+console.log(typeof alive)
