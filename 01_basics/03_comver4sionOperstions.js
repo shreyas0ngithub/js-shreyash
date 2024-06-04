2bc03e8184c0636f2ecbc55143ab7d443b46d640
shreyas0ngithub <shreyas.shastrakar07@gmail.com>
2024-06-04 10:07:46 +0000

    day2-2

---
+++ 01_basics/03_comver4sionOperstions.js
@@ -0,0 +1,33 @@
+let  marks = true
+
+let valueinNumber = Number(marks)
+//console.log(marks)
+//console.log(valueinNumber)
+
+//"33"-> 33
+//"33abc"-> NaN(Not a Number)
+//null -> 0
+//undefined -> NaN
+//true -> 1 ; false-> 0
+
+//console.log(valueinNumber)
+let isLoggedIn = 1
+
+let valueinBoolean = Boolean(isLoggedIn)
+
+
+
+//console.log(isLoggedIn)
+//console.log(valueinBoolean)
+
+//"abc" -> true
+//""-> false
+//null-> false
+//uindefined -> false
+//0 -> false ; 1-> true
+
+let someVar = 45 
+let stringVar = String(someVar)
+console.log(someVar)
+console.log(stringVar)
+console.log(typeof stringVar)
