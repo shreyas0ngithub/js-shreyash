a34242a9e5920a3655e4509bc2f822f4011ebe2c
shreyas0ngithub <shreyas.shastrakar07@gmail.com>
2024-06-08 12:30:55 +0000

    d5mod

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,13 +1,14 @@
-//const tinderUser = new Object()
-const tinderUser = {} //singleton object
+//const tinderUser = new Object()  //singleton object
+const tinderUser = {} //not a singleton obj 
 
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
 tinderUser.isLoggedin = false
 
-//console.log(tinderUser)
+// console.log(tinderUser)
 
-const regularUser = {
+
+const regularUser = {             //nested objects
     email: "some@gmail.com",
     fullname: {
         userfullname: {
@@ -17,37 +18,79 @@ const regularUser = {
     }
 }
 
-//console.log(regularUser.fullname.userfullname.firstname)
-
+//console.log(regularUser.fullname?.userfullname.firstname)  // ? is optional, it is used to confirm if key is present 
+                                                           //is present in the object or not
+//combining objects
 const obj1 = {1:"a",2:"b"}
 const obj2 = {3:"c",4:"d"}
 const obj4 = {5:"a",6:"b"}
 
 //const obj3 = {obj1, obj2}
-//const obj3 = Object.assign({}, obj1, obj2,obj4)
+// const obj3 = Object.assign({}, obj1, obj2,obj4)   //assigns values of ob1,obj2,obj4(source objects) to {}-> target obj
 
 const obj3 = {...obj1, ...obj2}
 //console.log(obj3)
 
+//how values in objects come from database
+// const Users = [{
+//     id: 1,
+//     email: "s@gmail.com"
+// },
+// {
+//     id: 21,
+//     email: "s@gmail.com"
+// },
+// {
+//     id: 13,
+//     email: "s@gmail.com"
+// },{
+//     id: 41,
+//     email: "s@gmail.com"
+// },{
+//     id: 51,
+//     email: "s@gmail.com"
+// },{
+//     id: 61,
+//     email: "s@gmail.com"
+// },{
+//     id: 1,
+//     email: "s@gmail.com"
+// }]
 
-const Users = {
-    id: 1,
-    email: "s@gmail.com"
-}
+// console.log(Users[1].email)
 
-// console.log(Object.keys(tinderUser))
-// console.log(Object.values(tinderUser))
-// console.log(Object.entries(tinderUser))
+
+//console.log(Object.keys(tinderUser))   //array of all keys
+//console.log(Object.values(tinderUser))   //array 0f all values
+//console.log(Object.entries(tinderUser))     //ar5ray pf all key-value pairs
 
 // console.log(tinderUser.hasOwnProperty('isLoggedin'))
 
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//Destructuring
+//Destructuring objects
 
-const course = {
-    coursename: "js in hindi",
-    price: "999",
-    courseInstructor: "hitesh"
-}
+// const course = {
+//     coursename: "js in hindi",
+//     price: "999",
+//     courseInstructor: "hitesh"
+// }
  
+// course.courseInstructor
+//const {courseInstructor} = course  //better to use for multiple console.log
+//console.log(courseInstructor)
+
+//also
+const {courseInstructor : Instructor} = course
+console.log(Instructor) 
+
+//json
+// {
+//     "name" :"shreyas",
+//     "coursename" : "js in hindi",
+//     "cost" : "free"
+// }
+
+//api iin array form
+
+
