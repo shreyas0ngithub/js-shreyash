92e3c5170e56c8bd6cbe12ed1f48d7be6034918c
shreyas0ngithub <shreyas.shastrakar07@gmail.com>
2024-06-11 10:32:05 +0000

    day08

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,47 @@
+// const userEmail = "shreyas@gmail.com"
+// const userEmail = ""   //empty string(false)
+const userEmail = []  //empty array(true)
+
+
+
+if(userEmail){
+    // console.log("got user email")
+} else{
+    // console.log("Don't have user email ")
+}
+
+
+// falsy values (values that are assumed to be false)
+// => false, 0, -0(neg zero), BigInt 0n, "", nll, undefined, Nan(not a number)
+
+//truthy values (values that are assumed to be true)
+// =>"0", 'false', " ", [], {}empty obj, function(){}empty function)
+
+if(userEmail.length === 0){
+    // console.log("Array is empty")
+}
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length === 0){
+    // console.log("Object is empty")
+}
+
+/********************************************************************************** */
+//Nullish Coalescing Operator(??) : null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1  = null ?? 10 ?? 20
+
+console.log(val1)
+
+/********************************************************************************** */
+// Ternary Operator 
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ?  console.log("more than 80") : console.log("less tha n 80")
---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+// let index = 0
+// while (index <=10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index +2
+// }
+
+ let myArray = ["flash", "batman", "superman"]
+ let arr = 0
+
+
+ while (arr < myArray.length) {
+    console.log(`${myArray[arr]}`);
+    arr++
+ }
+
+do {
+    
+} while (G);
