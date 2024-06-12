45f861b60d3d541ff0e4acbffe13b7426fa4bb7c
shreyas0ngithub <shreyas.shastrakar07@gmail.com>
2024-06-12 07:10:42 +0000

    day09.2

---
+++ 05_iterations/seven.js
@@ -0,0 +1,8 @@
+//map() - much similar to filter
+const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+const newnums = myNumbers.map((num)=> num+20)
+
+console.log(newnums);
+
+/*************Chaining************* */
